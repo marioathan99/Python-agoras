0bd319561a53019f5896314879bfc1edcd9d6b48
marioathan99 <marioathanasiou99@gmail.com>
2025-05-24 16:16:38 +0300

    Initial Commit

---
+++ hypotenuse.py
@@ -0,0 +1,10 @@
+# Let's go! 💖
+username = input ('Enter your name: ')
+print ('Hey', username, "I\'m Pythagoras")
+age = int(input('What is your age? '))
+print('Oh it\'s nice that you\'re',age,". That's the proper age to show you my theorem")
+print('My theorem actually looks like this: (a**2 + b**2)**0.5 ~ the 0.5 serves as the root of the whole thing')
+a = int(input("So, give me one number for a: "))
+b = int(input("You're very smart, now give me another one for b: "))
+c = (a**2 + b**2)**0.5
+print('Now that I can calculate this quickly on my virtual mind, the hepotenuse is: ', c, "!")
